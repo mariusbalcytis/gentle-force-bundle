2039d4aac7c83e5fca30b9d749ac497f0dabbf64
Marius Balčytis <marius.balcytis@gmail.com>
2017-06-13 08:03:08 +0300

    Fixes code style violation

--- src/Listener/RequestMatcher.php
+++ src/Listener/RequestMatcher.php
@@ -6,7 +6,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 class RequestMatcher
 {
-
     public function matches(ListenerConfiguration $configuration, Request $request)
     {
         return preg_match($configuration->getPathPattern(), $request->getRequestUri()) === 1;
